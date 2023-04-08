04f5bfd5b372fafca0809787e69ce3b9164f78d0
GinaFerl <ginaferlindawati@gmail.com>
2023-04-08 08:58:34 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/ausapp/AlphabetActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ausapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlphabetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alphabet)
+    }
+}
---
+++ app/src/main/java/com/example/ausapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.ausapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bottomNav : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        loadFragment(HomeFragment())
+        bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(HomeFragment())
+                    true
+                }
+                R.id.quiz -> {
+                    loadFragment(QuizFragment())
+                    true
+                }
+                else -> {throw AssertionError()}
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
+    }
+}
